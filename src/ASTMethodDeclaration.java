ef03ca98bc6434a849adc907ee98371cd3e7ceb5
EstevesAndre <estevesandre1@hotmail.com>
2019-05-15 12:36:18 +0100

    update

--- src/ASTMethodDeclaration.java
+++ src/ASTMethodDeclaration.java
@@ -3,9 +3,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 /* Gen /* JavaCCOptions:MULTI=true,NODE_USES_PARSER=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
-public
-class ASTMethodDeclaration extends SimpleNode {
-public  int global_order = 1;
+public class ASTMethodDeclaration extends SimpleNode {
+  public int global_order = 1;
   public HashMap<String, STEntry> symtbl = new HashMap<String, STEntry>();
 
   public String id;
@@ -22,40 +21,42 @@ public  int global_order = 1;
     return "method " + id;
   }
 
-  void triggerSemanticAnalysis() throws SemanticException
-  {
+  void triggerSemanticAnalysis() throws SemanticException {
     List<String> args = new ArrayList<>();
     String type = null;
-    SimpleNode argsNode = (SimpleNode)children[1];
-    if(children[0] instanceof ASTType){
-      type = ((ASTType)(children[0])).getType();
+    SimpleNode argsNode = (SimpleNode) children[1];
+    if (children[0] instanceof ASTType) {
+      type = ((ASTType) (children[0])).getType();
     }
 
-    if(argsNode.children != null)
-      for(Node arg : argsNode.children){
-        if(arg instanceof ASTArg){
-          args.add(((ASTArg)arg).getType());
+    if (argsNode.children != null)
+      for (Node arg : argsNode.children) {
+        if (arg instanceof ASTArg) {
+          args.add(((ASTArg) arg).getType());
         }
       }
 
-    STFunc func = new STFunc(-1, id,type,line,column,args);
-    if(!addToSymbolTable(func.getKeyName(),func)){
+    STFunc func = new STFunc(-1, id, type, line, column, args);
+    if (!addToSymbolTable(func.getKeyName(), func)) {
       throw new SemanticException("Function already defined: " + id + " at line " + line + ", column " + column + ".");
     }
   }
 
   String getJasmin() {
     String out = ".method public " + id + "(";
-    
+
     ASTArgs args = (ASTArgs) children[1];
 
-    for(Node arg : args.children) {
-      out += getJasminType(((ASTType)(((ASTArg)(arg)).children[0])).getType());
+    for (Node arg : args.children) {
+      out += getJasminType(((ASTType) (((ASTArg) (arg)).children[0])).getType());
     }
 
-    out += ")"+ getJasminType(((ASTType)(children[0])).getType()) + "\n";
+    out += ")" + getJasminType(((ASTType) (children[0])).getType()) + "\n";
 
     return out;
   }
 }
-/* JavaCC - OriginalChecksum=4db4e7d00203d02b3e258a10941c8c0e (do not edit this line) */
+/*
+ * JavaCC - OriginalChecksum=4db4e7d00203d02b3e258a10941c8c0e (do not edit this
+ * line)
+ */
